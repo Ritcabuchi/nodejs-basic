f4876610f9a4760b9331dc42dbeb5a0fda2fdf66
ritcabuchi <ridwanritcabuchi@gmail.com>
2018-11-15 00:54:21 +0700

    Use express.static

--- src/app.js
+++ src/app.js
@@ -1,12 +1,21 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const todos = require('./routes/todos');
 const app = express()
 const port = process.env.PORT || 3000
 
+//parse a
+app.use(express.urlencoded({ extended: true}));
 
+app.use(express.json());
+//logger
+if(app.get('env') === 'development'){
+}
+app.use(express.static('public'));
+//route
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
  });
 
 app.use('/todos', todos);
